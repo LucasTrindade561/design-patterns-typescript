b2817f7a16a9d0c4698a3a811673335cd736b11d
Lucas Trindade <nh.lucass@gmail.com>
2022-01-24 19:59:09 -0300

    strategy and behavioural

---
+++ src/behavioural/strategy/index.ts
@@ -0,0 +1,15 @@
+import { DefaultDiscount } from './shopping-cart/default-discount';
+import { ECommerceShoppingCart } from './shopping-cart/ecommerce-shopping-cart';
+import { NewDiscount } from './shopping-cart/new-discount';
+
+const shoppingCart = new ECommerceShoppingCart();
+shoppingCart.discount = new NewDiscount();
+// shoppingCart.discount = new DefaultDiscount();
+shoppingCart.addProduct({ name: 'Product 1', price: 50 });
+shoppingCart.addProduct({ name: 'Product 2', price: 50 });
+shoppingCart.addProduct({ name: 'Product 1', price: 50 });
+shoppingCart.addProduct({ name: 'Product 2', price: 50 });
+shoppingCart.addProduct({ name: 'Product 1', price: 50 });
+shoppingCart.addProduct({ name: 'Product 2', price: 50 });
+console.log(shoppingCart.getTotal());
+console.log(shoppingCart.getTotalWithDiscount());
